fa78be801153615a2ad61a7ecbb5398a1f922440
Lucas Aguirre <lucas_aguirre1714@hotmail.com>
2023-03-02 15:14:30 -0300

    Garbarino page beta version.

---
+++ index.js
@@ -0,0 +1,17 @@
+class Tvs{
+    constructor(model, picture, price){
+        this.model = model
+        this.picture = picture
+        this.price = price
+    }
+}
+
+let philips6800 = new Tvs ('Smart Tv Philips 6800 Series 43pfd6825/77 Led Full HD 43 110v/240v', './assets/tv-assets/smart-tv-philips-6800-series-43pfd6825-77-led-full-hd-43-110v-240v.png', '$109.999')
+
+let samsungT5300 = new Tvs ('Televisor Samsung Smart Tv Full HD T5300 2020', './assets/tv-assets/televisor-samsung-smart-tv-43-full-hd-t5300-2020.png', '$119.999')
+
+let bghB3219k5 = new Tvs ('Televisor Smart Tv BGH B3219k5 Led HD 32 220v', './assets/tv-assets/televisor-smart-tv-bgh-b3219k5-led-hd-32-220v.png', '$49.999')
+
+let samsungSeries7 = new Tvs('Smart Tv Samsung Series 7 UN50AU7000GCZB Led 4K 50" 220v', './assets/tv-assets/smart-tv-samsung-series-7-un50au7000gczb-led-4k-50--220v---240v.png', '$127.999')
+
+let kanjiTcl = new Tvs('Smart TV Led TCL L55P735-F Google TV 55" 4K ULTRA HD', './assets/tv-assets/smart-tv-kanji-60--led-qhd-4k-9809b-13002.png', '$149.999')
